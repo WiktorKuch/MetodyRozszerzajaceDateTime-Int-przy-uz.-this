dc90e8e83181f39b524815e13391db00bb257fec
WiktorKuch <wiktor.kucharek1997@gmail.com>
2022-12-05 17:26:54 +0100

    metody rozszerzajace dateTime Int przy uzyciu this

---
+++ MetodyRozszerzajace/MetodyRozszerzajace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MetodyRozszerzajace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime now = DateTime.Now;
+            DateTime before = now.Subtract(new TimeSpan(7,0,0,0));
+            DateTime after = now.AddDays(7);
+
+            bool isDateBetween = Utils.IsDateBetween(now,before,after); // metoda sprawdza ze data mieści sie w przedziale 
+
+            bool isDatebetween2 = now.IsBetween(before,after);  // na wartosci typu datetime wywołamy nasza metode - poniewaz klasa jej nie posiada- uzycie extension methods
+                                                                // IsBetween(); pierwotnie nie istnieje - jest to nasza metoda rozszerzajaca
+
+
+            int value = 2;
+            value.Squared();
+
+
+
+        }
+    }
+}
---
+++ MetodyRozszerzajace/MetodyRozszerzajace/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodyRozszerzajace
+{
+    public static class Utils
+    {
+
+        public static bool IsDateBetween(DateTime date,DateTime from , DateTime to)   //1.data ktora chcemy sprawdzic 
+        {
+            return date >= from && date <= to;
+        }
+
+        public static bool IsBetween(this DateTime date, DateTime from, DateTime to) // slowo kluczone this - aby metoda byla m.rozszerzajaca dla typu datetime
+        {
+            return date >= from && date <= to;                                     // trzeba wtedy zmienic klase na static
+        }
+
+        public static int Squared(this int value)  // metoda rozsz. dla typu int 
+        {
+            return value * value;
+        }
+
+
+    }
+}
